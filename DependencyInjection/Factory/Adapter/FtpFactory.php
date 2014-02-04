334786998c4505ceab99caea99e6537b9fa0209a
Jim Schmid <jim.schmid@1up.io>
2014-02-04 20:55:08 +0100

    Ftp and Sftp adapter factories.

---
+++ DependencyInjection/Factory/Adapter/FtpFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class FtpFactory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'ftp';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $definition = $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.ftp'))
+            ->replaceArgument(0, $config)
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('host')->isRequired()->end()
+
+                ->integerNode('port')->defaultValue(21)->end()
+                ->scalarNode('username')->defaultNull()->end()
+                ->scalarNode('password')->defaultNull()->end()
+                ->scalarNode('root')->defaultNull()->end()
+                ->booleanNode('ssl')->defaultFalse()->end()
+                ->integerNode('timeout')->defaultValue(90)->end()
+                ->scalarNode('permPrivate')->defaultValue(0000)->end()
+                ->scalarNode('permPublic')->defaultNull(0744)->end()
+                ->booleanNode('passive')->defaultTrue()->end()
+            ->end()
+        ;
+    }
+}
---
+++ DependencyInjection/Factory/Adapter/SftpFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class SftpFactory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'sftp';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $definition = $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.sftp'))
+            ->replaceArgument(0, $config)
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('host')->isRequired()->end()
+
+                ->integerNode('port')->defaultValue(22)->end()
+                ->scalarNode('username')->defaultNull()->end()
+                ->scalarNode('password')->defaultNull()->end()
+                ->integerNode('timeout')->defaultValue(90)->end()
+                ->scalarNode('root')->defaultNull()->end()
+                ->scalarNode('privateKey')->defaultNull()->end()
+                ->scalarNode('permPrivate')->defaultValue(0000)->end()
+                ->scalarNode('permPublic')->defaultNull(0744)->end()
+            ->end()
+        ;
+    }
+}
