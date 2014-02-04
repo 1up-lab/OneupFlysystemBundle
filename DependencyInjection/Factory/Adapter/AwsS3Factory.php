b43351afaa8d8ec277e4ed9aa99529aa27617b55
Jim Schmid <jim.schmid@1up.io>
2014-02-04 20:20:32 +0100

    AwsS3 support, without the option argument yet.

---
+++ DependencyInjection/Factory/Adapter/AwsS3Factory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class AwsS3Factory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'awss3';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $definition = $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.zip'))
+            ->replaceArgument(0, new Reference($config['client']))
+            ->replaceArgument(1, $config['bucket'])
+            ->replaceArgument(2, $config['prefix'])
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('client')->isRequired()->end()
+                ->scalarNode('bucket')->isRequired()->end()
+                ->scalarNode('prefix')->defaultNull()->end()
+            ->end()
+        ;
+    }
+}
