3352d5f6916d659a01c3e8fd1087a4484147a7e6
Jim Schmid <jim.schmid@1up.io>
2014-02-04 20:11:58 +0100

    Added ZipFactory.

---
+++ DependencyInjection/Factory/Adapter/ZipFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class ZipFactory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'zip';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $archive = !is_null($config['archive']) ? new Reference($config['archive']) : null;
+
+        $definition = $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.zip'))
+            ->replaceArgument(0, $config['location'])
+            ->replaceArgument(1, $archive)
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('location')->isRequired()->end()
+                ->scalarNode('archive')->defaultNull()->end()
+            ->end()
+        ;
+    }
+}
