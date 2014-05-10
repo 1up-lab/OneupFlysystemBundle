fad3b8c1046aa166eb34f2fad520c1db23458f56
Jim Schmid <jim.schmid@1up.io>
2014-05-10 19:53:25 +0200

    Added NullAdapter.
    Closes #10.

---
+++ DependencyInjection/Factory/Adapter/NullAdapterFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class NullAdapterFactory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'nulladapter';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.nulladapter'))
+            ->replaceArgument(0, $config)
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+            ->end()
+        ;
+    }
+}
---
+++ Resources/doc/adapter_nulladapter.md
@@ -0,0 +1,14 @@
+# Use the NullAdapter
+
+According to the Flysystem documentation the NullAdapter acts like `/dev/null`, you can only write to it.
+Reading from it is never possible. Can be useful for writing tests.
+
+```yml
+oneup_flysystem:
+    adapters:
+        nulladapter: ~
+```
+
+## More to know
+* [Create and use your filesystem](filesystem_create.md)
+* [Add a cache](filesystem_cache.md)
