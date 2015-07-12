88215b5723b5df89917e0c7355b49bca8d21acc4
Vu Nguyen <yellow1912@gmail.com>
2015-07-12 13:12:05 +0700

    Add support for Azure

---
+++ DependencyInjection/Factory/Adapter/AzureFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Symfony\Component\DependencyInjection\Reference;
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class AzureFactory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'azure';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $definition = $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.azure'))
+            ->replaceArgument(0, new Reference($config['client']))
+            ->replaceArgument(1, $config['container'])
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+                ->scalarNode('client')->isRequired()->end()
+                ->scalarNode('container')->isRequired()->end()
+            ->end()
+        ;
+    }
+}
