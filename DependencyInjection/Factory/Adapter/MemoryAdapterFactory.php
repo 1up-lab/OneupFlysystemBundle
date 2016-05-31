a81eba07c71a30aa81ef97b4e35650c1df557f58
Jim Schmid <jim.schmid@1up.io>
2016-05-31 12:49:26 +0200

    Added support for league/flysystem-memory.
    Addresses #105.

---
+++ DependencyInjection/Factory/Adapter/MemoryAdapterFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+
+class MemoryAdapterFactory implements AdapterFactoryInterface
+{
+    public function getKey()
+    {
+        return 'memory';
+    }
+
+    public function create(ContainerBuilder $container, $id, array $config)
+    {
+        $container
+            ->setDefinition($id, new DefinitionDecorator('oneup_flysystem.adapter.memory'))
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node)
+    {
+        $node
+            ->children()
+            ->end()
+        ;
+    }
+}
