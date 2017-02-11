61231f00a6b47949f37c9b725cae6c7c5bd35601
Kirilenko Georgii <g.kirilenko@dengionline.com>
2017-02-11 23:17:00 +0300

    Fix FTP_BINARY notice when ftp module not loaded

--- DependencyInjection/Factory/Adapter/FtpFactory.php
+++ DependencyInjection/Factory/Adapter/FtpFactory.php
@@ -37,7 +37,7 @@ class FtpFactory implements AdapterFactoryInterface
                 ->scalarNode('permPrivate')->defaultValue(0000)->end()
                 ->scalarNode('permPublic')->defaultNull(0744)->end()
                 ->booleanNode('passive')->defaultTrue()->end()
-                ->scalarNode('transferMode')->defaultValue(FTP_BINARY)->end()
+                ->scalarNode('transferMode')->defaultValue(defined('FTP_BINARY') ? FTP_BINARY : null)->end()
                 ->scalarNode('systemType')->defaultNull()->end()
                 ->booleanNode('ignorePassiveAddress')->defaultNull()->end()
                 ->booleanNode('recurseManually')->defaultFalse()->end()
