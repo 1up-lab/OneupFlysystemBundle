b00114a96becb39ef06e163c1233190a5de864ad
Michael Babker <michael.babker@gmail.com>
2018-11-30 13:09:56 -0600

    Update bundle configuration for Symfony 4.2 change

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,8 +20,14 @@ class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('oneup_flysystem');
+        $treeBuilder = new TreeBuilder('oneup_flysystem');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('oneup_flysystem');
+        }
 
         $this->addCacheSection($rootNode);
         $this->addAdapterSection($rootNode);
