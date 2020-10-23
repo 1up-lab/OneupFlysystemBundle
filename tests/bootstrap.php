47cf95b6f87ed0bdb3bac0e83543fa885d90ba79
David Greminger <david.greminger@1up.io>
2020-10-23 12:00:52 +0200

    Add github actions and phpstan config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,25 +2,14 @@
 
 declare(strict_types=1);
 
-$loader = @include __DIR__ . '/../vendor/autoload.php';
-if (!$loader) {
-    die(<<<'EOT'
-You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install
-EOT
-    );
+if (!($loader = @include __DIR__ . '/../vendor/autoload.php')) {
+    echo <<<'EOT'
+You need to install the project dependencies using Composer:
+$ wget http://getcomposer.org/composer.phar
+OR
+$ curl -s https://getcomposer.org/installer | php
+$ php composer.phar install --dev
+$ phpunit
+EOT;
+    exit(1);
 }
-
-passthru(sprintf('rm -rf %s/App/cache', __DIR__));
-spl_autoload_register(function ($class) {
-    if (0 === strpos($class, 'Oneup\\FlysystemBundle\\')) {
-        $path = __DIR__ . '/../' . implode('/', array_slice(explode('\\', $class), 2)) . '.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-
-        return true;
-    }
-});
