d782e06404a6b036687b6e85e15af0701381bbc8
Daniele Pastori <fullbl@mail.com>
2021-02-13 10:29:34 +0100

    create cached adapter from factory

---
+++ src/DependencyInjection/Factory/Adapter/CachedFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
+
+use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CachedFactory implements AdapterFactoryInterface
+{
+    public function getKey(): string
+    {
+        return 'cached';
+    }
+
+    public function create(ContainerBuilder $container, string $id, array $config): void
+    {
+        $container
+            ->setDefinition($id, new ChildDefinition('oneup_flysystem.adapter.cached'))
+        ;
+    }
+
+    public function addConfiguration(NodeDefinition $node): void
+    {
+    }
+}
