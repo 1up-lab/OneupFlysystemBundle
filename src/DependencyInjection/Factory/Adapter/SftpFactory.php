70dc1957c6f99fa194428f9864e953373afe8168
David Greminger <bytehead@users.noreply.github.com>
2021-05-12 08:52:59 +0200

    Fix SFTP adapter configuration (#242)

--- src/DependencyInjection/Factory/Adapter/SftpFactory.php
+++ src/DependencyInjection/Factory/Adapter/SftpFactory.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
 
+use League\Flysystem\PhpseclibV2\SftpConnectionProvider;
 use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class SftpFactory implements AdapterFactoryInterface
 {
@@ -18,10 +20,17 @@ class SftpFactory implements AdapterFactoryInterface
 
     public function create(ContainerBuilder $container, string $id, array $config): void
     {
+        $root = $config['options']['root'];
+        unset($config['options']['root']);
+
         $container
             ->setDefinition($id, new ChildDefinition('oneup_flysystem.adapter.sftp'))
-            ->replaceArgument(0, $config['connectionProvider'])
-            ->replaceArgument(1, $config['root'])
+            ->replaceArgument(0, (new Definition(SftpConnectionProvider::class))
+                ->setFactory([SftpConnectionProvider::class, 'fromArray'])
+                ->addArgument($config['options'])
+                ->setShared(false)
+            )
+            ->replaceArgument(1, $root)
             ->replaceArgument(2, $config['visibilityConverter'])
             ->replaceArgument(3, $config['mimeTypeDetector'])
         ;
@@ -31,8 +40,22 @@ class SftpFactory implements AdapterFactoryInterface
     {
         $node
             ->children()
-                ->scalarNode('connectionProvider')->isRequired()->end()
-                ->scalarNode('root')->isRequired()->end()
+                ->arrayNode('options')->isRequired()
+                    ->children()
+                        ->scalarNode('host')->isRequired()->end()
+                        ->scalarNode('username')->isRequired()->end()
+                        ->scalarNode('password')->defaultNull()->end()
+                        ->scalarNode('privateKey')->defaultNull()->end()
+                        ->scalarNode('passphrase')->defaultNull()->end()
+                        ->scalarNode('port')->defaultValue(22)->end()
+                        ->booleanNode('useAgent')->defaultFalse()->end()
+                        ->scalarNode('timeout')->defaultValue(10)->end()
+                        ->scalarNode('maxTries')->defaultValue(4)->end()
+                        ->scalarNode('hostFingerprint')->defaultNull()->end()
+                        ->scalarNode('connectivity checker')->defaultNull()->end()
+                        ->scalarNode('root')->isRequired()->end()
+                    ->end()
+                ->end()
                 ->scalarNode('visibilityConverter')->defaultNull()->end()
                 ->scalarNode('mimeTypeDetector')->defaultNull()->end()
             ->end()
