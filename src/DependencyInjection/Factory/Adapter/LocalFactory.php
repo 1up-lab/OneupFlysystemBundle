73887314cbc5414a7b60f7003bafc16118611969
David Greminger <david.greminger@1up.io>
2021-09-20 10:03:54 +0200

    Create VisibilityConverter dynamically based on options

--- src/DependencyInjection/Factory/Adapter/LocalFactory.php
+++ src/DependencyInjection/Factory/Adapter/LocalFactory.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Oneup\FlysystemBundle\DependencyInjection\Factory\Adapter;
 
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
 use Oneup\FlysystemBundle\DependencyInjection\Factory\AdapterFactoryInterface;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 class LocalFactory implements AdapterFactoryInterface
 {
@@ -19,11 +21,19 @@ class LocalFactory implements AdapterFactoryInterface
 
     public function create(ContainerBuilder $container, string $id, array $config): void
     {
+        $visibilityConverter = null;
+
+        if (isset($config['visibilityConverter']) && null !== $config['visibilityConverter']) {
+            $visibilityConverter = new Definition(PortableVisibilityConverter::class);
+            $visibilityConverter->setFactory([PortableVisibilityConverter::class, 'fromArray']);
+            $visibilityConverter->setArgument(0, $config['permissions'] ?? []);
+        }
+
         $container
             ->setDefinition($id, new ChildDefinition('oneup_flysystem.adapter.local'))
             ->setLazy($config['lazy'])
             ->replaceArgument(0, $config['location'])
-            ->replaceArgument(1, $config['visibilityConverter'])
+            ->replaceArgument(1, $visibilityConverter)
             ->replaceArgument(2, $config['writeFlags'])
             ->replaceArgument(3, $config['linkHandling'])
             ->replaceArgument(4, $config['mimeTypeDetector'])
@@ -36,7 +46,22 @@ class LocalFactory implements AdapterFactoryInterface
             ->children()
                 ->booleanNode('lazy')->defaultValue(false)->end()
                 ->scalarNode('location')->isRequired()->end()
-                ->scalarNode('visibilityConverter')->defaultNull()->end()
+                ->arrayNode('permissions')
+                    ->children()
+                        ->arrayNode('files')
+                            ->children()
+                                ->scalarNode('public')->defaultNull()->end()
+                                ->scalarNode('private')->defaultNull()->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('directories')
+                            ->children()
+                                ->scalarNode('public')->defaultNull()->end()
+                                ->scalarNode('private')->defaultNull()->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
                 ->scalarNode('writeFlags')->defaultValue(\LOCK_EX)->end()
                 ->scalarNode('linkHandling')->defaultValue(LocalFilesystemAdapter::DISALLOW_LINKS)->end()
                 ->scalarNode('mimeTypeDetector')->defaultNull()->end()
