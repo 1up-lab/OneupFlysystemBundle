6c4532b8bd6ceb9a53d8ee184831997d66f3145a
chris <chris@sky-scripts.de>
2025-10-31 16:11:36 +0100

    Update src/Resources/config/flysystem.php
    Co-authored-by: David Greminger <bytehead@users.noreply.github.com>

--- src/Resources/config/flysystem.php
+++ src/Resources/config/flysystem.php
@@ -8,10 +8,10 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set('oneup_flysystem.mount_manager', League\Flysystem\MountManager::class)
-    ->public()
-    ->args([
-        'filesystems',
-    ])
+        ->public()
+        ->args([
+            'filesystems',
+        ])
     ;
 
     $services->set('oneup_flysystem.filesystem', League\Flysystem\Filesystem::class)->public()->abstract()
